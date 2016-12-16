32781abd2d604fbfeb21547815c69ab48784d892
lsw <lsw_wsl@outlook.com>
2016-12-16 16:33:36 +0800

    东西四命算法第一部分

--- app/src/main/java/lsw/fengshui/MainActivity.java
+++ app/src/main/java/lsw/fengshui/MainActivity.java
@@ -2,9 +2,11 @@ package lsw.fengshui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.io.Console;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Dictionary;
@@ -32,7 +34,65 @@ public class MainActivity extends AppCompatActivity {
         setTextByRow(al1,strArray1);
         setTextByRow(al2,strArray2);
         setTextByRow(al3,strArray3);
+
+        int index = calculate(1982,true);
+
+        Log.d("index hexagra",Integer.toString(index));
+
+        int index2 = calculate(1983,true);
+
+        Log.d("index hexagra",Integer.toString(index2));
     }
+
+    //1984 下元甲子第一年
+    //一坎；二坤；三震；四巽；六乾；七兑；八艮；九离。余数是五的男性视为坤，女性则视为艮
+
+    //1864 上元甲子第一年
+    //1924 中元甲子第一年
+
+    int[] men = new int[]{1,4,7};
+    int[] women = new int[]{5,2,8};
+
+    private int calculate(int year, boolean isMale){
+
+        int yearCount = year - 1864;
+        if ( yearCount > 0){
+
+            int num = yearCount/60;
+            int mod = yearCount%60;
+
+            //0 上元 1中元 2下元
+            int whichiYuan = num%3;
+
+            int beginIndex = men[whichiYuan];
+            if (!isMale)
+            {
+                beginIndex = women[whichiYuan];
+            }
+
+            if(isMale)
+            {
+                int modIndex = mod%9;
+
+                //int guaIndex = 0;
+                int i=0;
+                while (i<modIndex){
+
+                    while (beginIndex < 1){
+                        beginIndex = 9;
+                    }
+                    beginIndex --;
+                    i++;
+                }
+
+                return beginIndex==0?9:beginIndex;
+            }
+        }
+
+        return 0;
+    }
+
+
 
     private Dictionary<String,String> getZiBaiDefault(){
         Dictionary<String,String> dicZiBai = new Hashtable<>();
